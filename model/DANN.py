76e790fef27b2a35a7dcc90ad3ccb7af99b10eea
julieDai <daijulie55@gmail.com>
2025-05-28 15:43:42 +0800

    Update DANN.py
    Replace Chinese comments with English ones.

--- model/DANN.py
+++ model/DANN.py
@@ -17,7 +17,7 @@ class DANDataset(Dataset):
     def __init__(self, data, celltype_list, label=None):
         self.data = data
         self.label = label
-        # 加上了position的维度2
+        # Added the position dimension, which has a size of 2
         self.data_lenth = data.shape[1]+2
 
         self.celltype_list = celltype_list
@@ -27,10 +27,10 @@ class DANDataset(Dataset):
         return self.data.shape[0]
 
     def __getitem__(self, index):
-        # 获取稀疏矩阵数据和标签数组
+        # Load sparse matrix data and corresponding label array
         input_data = self.data[index].toarray()
         input_data = torch.tensor(input_data, dtype=torch.float32).clone().detach()
-        # 拼接 [0, 0] 到 input_data (初始设置)
+        # # Append [0, 0] to input_data (initial setup)
         zero_position_label = torch.tensor([[0.0, 0.0]], dtype=torch.float32)
         input_data = torch.cat((input_data, zero_position_label), dim=1)
 
@@ -85,16 +85,16 @@ class Predictor(nn.Module):
     def forward(self, x):
         predicted_ratios = self.predictor_da(x)
         return predicted_ratios
-# 定义领域分类器（Domain Classifier）
+# Definition of the domain classifier
 class DomainClassifier(nn.Module):
     def __init__(self, input_size):
         super(DomainClassifier, self).__init__()
-        # 定义领域分类器的结构
+        # Define the architecture of the domain classifier
         self.discriminator_da = nn.Sequential(EncoderBlock(256, 128, 0.2),
                                               nn.Linear(128, 1),
                                               nn.Sigmoid())
     def forward(self, x):
-        # 前向传播计算领域分类结果
+        # Perform forward propagation to compute domain classification results
         discriminator_da = self.discriminator_da(x)
         return discriminator_da
 def trainDAN(ref_dataset, target_dataset, batch_size, shuffle, num_epoch, filepath, learning_rate):
@@ -102,9 +102,9 @@ def trainDAN(ref_dataset, target_dataset, batch_size, shuffle, num_epoch, filepa
 
     print("CUDA Available:", cuda_available)
 
-    # 设置设备
+    # Set computing device (GPU)
     device = torch.device("cuda:0")
-    # 将自定义数据集转换为 DataLoader 对象
+    # Wrap the custom dataset in a DataLoader
     ref_dataloder = torch.utils.data.DataLoader(ref_dataset, batch_size=batch_size, shuffle=shuffle, drop_last=True)
     target_dataloder = torch.utils.data.DataLoader(target_dataset, batch_size=batch_size, shuffle=True, drop_last=True)
 
@@ -113,14 +113,14 @@ def trainDAN(ref_dataset, target_dataset, batch_size, shuffle, num_epoch, filepa
 
     input_size = ref_dataset.data_lenth
     
-    # 创建模型实例并移动到 device 上
+    # Instantiate the model and transfer it to the designated device
     encoder = Encoder(input_size=input_size).to(device)
     domainclassifier = DomainClassifier(input_size=input_size).to(device)
 
     os.makedirs(f'{filepath}DANN_model/Model_parameters/', exist_ok=True)
     os.makedirs(f'{filepath}DANN_model/Model_result/', exist_ok=True)
 
-    #加载之前模型参数
+    # Load previously saved model parameters
     newly_encoder_model = get_newly_filename(f'{filepath}AE_model/Model_parameters/','encoder_model.pth')
     newly_domain_model = get_newly_filename(f'{filepath}DANN_model/Model_parameters/','domainclassifier_model.pth')
     encoder.load_state_dict(torch.load(f'{newly_encoder_model}'))
@@ -129,10 +129,10 @@ def trainDAN(ref_dataset, target_dataset, batch_size, shuffle, num_epoch, filepa
 
         domainclassifier.load_state_dict(torch.load(f'{newly_domain_model}'))
 
-    # 定义损失函数和优化器
+    # Define the loss function and the optimizer
     bce_loss_fn = nn.BCELoss()
     optimizer_domainclassifier = torch.optim.Adam(domainclassifier.parameters(), lr=learning_rate)
-    # 进行模型训练
+    # Perform model training
     num_epochs = num_epoch
     loss_list = []
     for epoch in range(num_epochs):
@@ -141,7 +141,7 @@ def trainDAN(ref_dataset, target_dataset, batch_size, shuffle, num_epoch, filepa
         encoder.train()
         domainclassifier.train()
 
-        total_loss_epoch = 0.0  # 用于记录每个 epoch 的总损失
+        total_loss_epoch = 0.0  # Record the total loss for each epoch
         sum_accuracy = 0.0
         total_loss_BCE_sum = 0.0
 
@@ -150,7 +150,7 @@ def trainDAN(ref_dataset, target_dataset, batch_size, shuffle, num_epoch, filepa
 
         for batch_idx, batch_ref in enumerate(ref_dataloder):
             batch_target = next(target_iter)
-            # 获取批次数据并移动到 device 上
+            # Retrieve batch data and move it to the device
             input_tensor_batch_ref, input_label_batch_ref = batch_ref
             input_tensor_batch_ref = input_tensor_batch_ref.to(device)
             input_label_batch_ref = input_label_batch_ref.to(device)
@@ -161,32 +161,32 @@ def trainDAN(ref_dataset, target_dataset, batch_size, shuffle, num_epoch, filepa
             input_tensor_batch_ref = input_tensor_batch_ref.squeeze(dim=1)
             input_tensor_batch1_target = input_tensor_batch1_target.squeeze(dim=1)
 
-            # 编码输入矩阵
+            # Encode the input matrix
             encoded_target = encoder(input_tensor_batch1_target)
             encoded_ref = encoder(input_tensor_batch_ref)
 
 
-            # 使用domainclassifier，并计算BCEloss损失
+            # Apply the domain classifier and calculate the BCE loss
             domainclassified_target = domainclassifier(encoded_target)
             domainclassified_ref = domainclassifier(encoded_ref)
             BCEloss = bce_loss_fn(domainclassified_target, target_label) + bce_loss_fn(domainclassified_ref, ref_label)
 
 
-            # 综合损失
+            # Compute the combined loss
             total_loss = BCEloss
-            total_loss_epoch += total_loss.item() # 累加每个 batch 的损失
+            total_loss_epoch += total_loss.item() # Add up the loss from each batch
             total_loss_BCE = BCEloss
 
             total_loss_BCE_sum += total_loss_BCE.item()
 
-            # 清空优化器的梯度，进行反向传播和优化
+            # Zero optimizer gradients, backpropagate, and optimize
             optimizer_domainclassifier.zero_grad()
             total_loss.backward()
 
-            # 分别进行优化
+            # Perform optimization for the domain classifier
             optimizer_domainclassifier.step()
 
-        # 输出每个 epoch 的平均损失
+        # Log the average loss for each epoch
         avg_accuracy = sum_accuracy / len(ref_dataloder)
         avg_loss_epoch = total_loss_epoch / len(ref_dataloder)
         avg_loss_BCE = total_loss_BCE_sum / len(ref_dataloder)
@@ -198,7 +198,7 @@ def trainDAN(ref_dataset, target_dataset, batch_size, shuffle, num_epoch, filepa
     x = list(range(2, len(loss_list) + 1))
     loss_list = loss_list[1:]
 
-    # 绘制 loss 曲线
+    # Plot the loss curve
     plt.plot(x, loss_list)
 
     folder_path = f'{filepath}DANN_model/Model_result/'
@@ -206,9 +206,9 @@ def trainDAN(ref_dataset, target_dataset, batch_size, shuffle, num_epoch, filepa
     L1_unique_filename =get_unique_filename(folder_path,L1_filename)
     plt.savefig(L1_unique_filename)
 
-    # 显示图表
+    # Display the plot
     plt.show()
-    # 训练完成后，保存模型参数
+    # Save model parameters after training
     encoder_model_unique = get_unique_filename(f'{filepath}DANN_model/Model_parameters/','encoder_model.pth')
     domainclassifier_model_unique = get_unique_filename(f'{filepath}DANN_model/Model_parameters/','domainclassifier_model.pth')
     torch.save(encoder.state_dict(), f'{encoder_model_unique}')
